d93ae046cc5aec55bd2325f145565a7a61a6b3cd
EvanWashkow <epwashkow@gmail.com>
2018-01-26 13:15:17 -0800

    Port the class framework class instance

---
+++ ClassFramework/Class.php
@@ -0,0 +1,113 @@
+<?php
+namespace ClassFramework;
+
+/**
+ * Defines a framework class that has and loads subcomponents
+ *
+ * By default, subcomponents are included from the folder with the same name as
+ * the class current class. To modify this behavior, extend this class, and then
+ * override the member functions appropriately.
+ *
+ * Example:
+ * MyClass.php                  // class MyClass extends \ClassFramework\Class
+ * MyClass/
+ *     MyClassSubcomponent1.php
+ *     MyClassSubcomponent2.php
+ *     MyClassSubcomponent3.php
+ */
+class Class
+{
+    
+    /***************************************************************************
+    *                           CLASS INFORMATION
+    ***************************************************************************/
+    
+    /**
+     * Return unique identifier for this class
+     *
+     * Useful for identifying object instances in an array, firing events, or
+     * WordPress handlers for JavaScript and CSS.
+     *
+     * @return string
+     */
+    public static function GetID()
+    {
+        return strtolower( str_replace( '\\', '.', get_called_class() ));
+    }
+    
+    
+    /**
+     * Retrieve the current class's name (minus the namespace)
+     *
+     * @return string
+     */
+    final protected static function getClassName()
+    {
+        $class = explode( '\\', get_called_class());
+        $class = array_pop( $class );
+        return $class;
+    }
+    
+    
+    /***************************************************************************
+    *                           SUBCOMPONENT LOADER
+    ***************************************************************************/
+    
+    /**
+     * Load PHP file from the sub-component directory
+     *
+     * @param string|array $query The component name(s), minus '.php'
+     */
+    final protected static function include( $query )
+    {
+        if ( is_array( $query )) {
+            $directory = static::getComponentDirectory();
+            foreach ( $query as $component ) {
+                require_once( "{$directory}/{$component}.php" );
+            }
+        }
+        elseif ( is_string( $query )) {
+            self::include( [ $query ] );
+        }
+    }
+    
+    
+    /***************************************************************************
+    *                          DIRECTORY INFORMATION
+    ***************************************************************************/
+    
+    
+    /**
+     * Get full path for the class's current working directory
+     *
+     * @return string Directory for this class
+     */
+    final protected static function getDirectory()
+    {
+        $class = new \ReflectionClass( get_called_class() );
+        return dirname( $class->getFileName() );
+    }
+    
+    
+    /**
+     * Get full path for the class's sub-components
+     *
+     * @return string Directory for this class's sub-components
+     */
+    final protected static function getComponentDirectory()
+    {
+        return static::getDirectory() . '/' . static::getComponentSubdirectory();
+    }
+    
+    
+    /**
+     * Get relative path for the class's sub-components
+     *
+     * @return string
+     */
+    protected static function getComponentSubdirectory()
+    {
+        return self::getClassName();
+    }
+}
+?>
